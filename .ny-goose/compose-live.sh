9cea1937233539139d490e7a820a3279c6db3962
jawwwr <jarielb96@gmail.com>
2019-11-23 09:03:35 +0800

    update pipeline

--- .ny-goose/compose-live.sh
+++ .ny-goose/compose-live.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-VOLUME=$PWD':/web-app/'
-NY_API_HOST=$(cat .ny-goose/ny-api-host.txt)
-NY_S3_BUCKET=$(cat .ny-goose/ny-s3-bucket.txt)
-NY_S3_LINK=$(cat .ny-goose/ny-s3-link.txt)
-CONTAINER_NAME=$(cat .ny-goose/ny-container-name.txt)
-
-rm -fr $PWD'/dist/*'
-
 curl -X POST -H 'Content-type: application/json' -s $GOOSE_SLACK_WEBHOOK -d '{
 	"blocks": [
 		{
