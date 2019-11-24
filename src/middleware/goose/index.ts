7d81857fd9294b33f3e052798270b14d0cee396a
jawwwr <jarielb96@gmail.com>
2019-11-24 09:29:15 +0800

    update

--- src/middleware/goose/index.ts
+++ src/middleware/goose/index.ts
@@ -1,29 +1,17 @@
 import { splitWiseToken } from '../../controller/goose/hooks';
 
 export async function GooseMiddleware(ctx :any, next :any) {
-  try {
-    const sw = await splitWiseToken();
-    console.log(sw)
-    if(sw && sw.status == 'unauthorized') {
-      ctx.body = {
-          success: false,
-          link: sw.value,
-          message: 'Unauthorized Splitwise',
-      }
-      return
-    }
-
-    ctx.state.splitwise = sw
-    await next()
-
-  } catch(e) {
-    ctx.status = 401
+  const sw = await splitWiseToken();
+  console.log(sw)
+  if(sw && sw.status == 'unauthorized') {
     ctx.body = {
-        Success: false,
         success: false,
-        data: e,
-        message: 'Some problem occured.',
+        link: sw.value,
+        message: 'Unauthorized Splitwise',
     }
     return
   }
+
+  ctx.state.splitwise = sw
+  await next()
 }
